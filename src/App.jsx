668622c98cf81e361153c14985bff413cc2e4f98
Sabal Adhikari <sadaladhikari@gmail.com>
2025-04-27 20:30:35 +0545

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,3 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import {QueryClient, QueryClientProvider} from '@tanstack/react-query'
 import { FetchMeal } from './component/FetchMeal.jsx'
